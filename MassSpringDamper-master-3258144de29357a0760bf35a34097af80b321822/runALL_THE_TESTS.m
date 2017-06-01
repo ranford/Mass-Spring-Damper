2e1f7a35e948a7795a747cea883dbe40eeb0a13c
Andy Campbell <ranford.b.crandal@gmail.com>
2017-06-01 14:08:25 -0400

    Add test running script

---
+++ MassSpringDamper-master-3258144de29357a0760bf35a34097af80b321822/runALL_THE_TESTS.m
@@ -0,0 +1,44 @@
+import matlab.unittest.TestRunner;
+import matlab.unittest.plugins.TAPPlugin;
+import matlab.unittest.plugins.TestReportPlugin;
+import matlab.unittest.plugins.CodeCoveragePlugin;
+import matlab.unittest.plugins.ToFile;
+
+
+try
+    ws = getenv('WORKSPACE');
+
+    src = fullfile(ws, 'source');
+    addpath(src);
+    
+    tests = fullfile(ws, 'tests');
+    suite = testsuite(tests);
+
+    % Create and configure the runner
+    runner = TestRunner.withTextOutput('Verbosity',3);
+    
+    % Add the TAP plugin   
+    tapFile = fullfile(ws, 'testResults.tap');
+    runner.addPlugin(TAPPlugin.producingVersion13(ToFile(tapFile)));
+    
+    % Add the TestReportPlugin
+    % pdf
+    pdfFile = fullfile(ws, 'TestReport.pdf');
+    runner.addPlugin(TestReportPlugin.producingPDF(pdfFile));
+    
+    % html
+    htmlFolder = fullfile(ws, 'testresults');
+    runner.addPlugin(TestReportPlugin.producingHTML(htmlFolder,'IncludingCommandWindowText', true));
+    
+    
+    % Add the CodeCoveragePlugin
+    srcFolder = fullfile(ws, 'source');
+    runner.addPlugin(CodeCoveragePlugin.forFolder(srcFolder));
+    
+    
+    results = runner.run(suite)
+catch e
+    disp(getReport(e,'extended'));
+    exit(1);
+end
+exit;
