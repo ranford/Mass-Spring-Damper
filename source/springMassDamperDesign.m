6ebb1e98c2320cd90f7e9170f6623e40b953d94c
Andy Campbell <ranford.b.crandal@gmail.com>
2017-06-06 19:22:16 -0400

    change design

--- source/springMassDamperDesign.m
+++ source/springMassDamperDesign.m
@@ -3,4 +3,4 @@ function design = springMassDamperDesign
 m = 1500; % Need to know the mass to determine critical damping
 
 design.k = 5e6;                   % Spring Constant
-design.c = 1e3;%2*m*sqrt(design.k/m);  % Damping Coefficient to be critically damped
+design.c = 1e4;% 2*m*sqrt(design.k/m); % Damping Coefficient to be critically damped
--- tests/designTest.m
+++ tests/designTest.m
@@ -1,11 +1,9 @@
 function tests = designTest
-
 tests = functiontests(localfunctions); 
-
 end
 
 function testSettlingTime(testCase) 
-% Test that the system settles to within 0.001 of zero under 2 seconds.
+%%Test that the system settles to within 0.001 of zero under 2 seconds.
 
 import matlab.unittest.diagnostics.FigureDiagnostic;
 
@@ -16,12 +14,12 @@ testCase.addTeardown(@close, f);
 
 positionAfterSettling = position(time > .002);
 
-% For this example, verify the first value after the settling time.
+%For this example, verify the first value after the settling time.
 verifyLessThan(testCase, abs(positionAfterSettling), 2, FigureDiagnostic(f));
 end
 
 function testOvershoot(testCase)
-% Test to ensure that overshoot is less than 0.01
+ %Test to ensure that overshoot is less than 0.01
 
 import matlab.unittest.diagnostics.FigureDiagnostic;
 
@@ -32,6 +30,18 @@ f = plotResponse(time, position);
 testCase.addTeardown(@close, f);
 
 verifyLessThan(testCase, overshoot, 0.01, FigureDiagnostic(f));
+end
+
+function idealTest(testCase)
+
+setup
+
+exercise
+
+verify
+
+teardown
+
 end
 
 
