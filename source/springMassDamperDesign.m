036ac5028b7488c90e33846ac116f872e70714f3
Andy Campbell <ranford.b.crandal@gmail.com>
2017-06-07 16:00:44 -0400

    bad test

--- source/springMassDamperDesign.m
+++ source/springMassDamperDesign.m
@@ -3,4 +3,4 @@ function design = springMassDamperDesign
 m = 1500; % Need to know the mass to determine critical damping
 
 design.k = 5e6;                   % Spring Constant
-design.c = 2*m*sqrt(design.k/m); % Damping Coefficient to be critically damped
+design.c = 1e5%2*m*sqrt(design.k/m); % Damping Coefficient to be critically damped
