ac304363482c02766846a73fb7b38b2385b6debb
Andy Campbell <ranford.b.crandal@gmail.com>
2017-09-22 11:00:49 -0400

    Update designTest.m

--- tests/designTest.m
+++ tests/designTest.m
@@ -5,40 +5,19 @@ end
 function testSettlingTime(testCase) 
 %%Test that the system settles to within 0.001 of zero under 2 seconds.
 
-import matlab.unittest.diagnostics.FigureDiagnostic;
-
 [position, time] = simulateSystem; 
-
-f = plotResponse(time, position);
-testCase.addTeardown(@close, f);
 
 positionAfterSettling = position(time > .002);
 
 %For this example, verify the first value after the settling time.
-verifyLessThan(testCase, abs(positionAfterSettling), 2, FigureDiagnostic(f));
+verifyLessThan(testCase, abs(positionAfterSettling), 2);
 end
 
 function testOvershoot(testCase)
  %Test to ensure that overshoot is less than 0.01
 
-import matlab.unittest.diagnostics.FigureDiagnostic;
-
 [position, time] = simulateSystem;
 overshoot = max(position);
 
-f = plotResponse(time, position);
-testCase.addTeardown(@close, f);
-
-verifyLessThan(testCase, overshoot, 0.01, FigureDiagnostic(f));
-end
-
-
-function fig = plotResponse(varargin)
-fig = figure('Visible','off');
-fig.CreateFcn = @(o,e) set(o,'Visible','on');
-ax = axes('Parent',fig);
-plot(ax, varargin{:},'LineWidth',3);
-title('Response')
-xlabel('Time')
-ylabel('Position')
+verifyLessThan(testCase, overshoot, 0.01);
 end
