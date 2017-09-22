7f49d05fd1a8aa8f9bfd9c51896f8ccf882865c6
Andy Campbell <ranford.b.crandal@gmail.com>
2017-09-22 10:30:51 -0400

    Simplify the runner

--- runALL_THE_TESTS.m
+++ runALL_THE_TESTS.m
@@ -1,11 +1,8 @@
 try
     import('matlab.unittest.TestRunner');
     import('matlab.unittest.plugins.TAPPlugin');
-    import('matlab.unittest.plugins.XMLPlugin');
-    import('matlab.unittest.plugins.TestReportPlugin');
-    import('matlab.unittest.plugins.CodeCoveragePlugin');
     import('matlab.unittest.plugins.ToFile');
-    import('matlab.unittest.plugins.codecoverage.CoberturaFormat');
+
     
     ws = getenv('WORKSPACE');
     
@@ -17,37 +14,11 @@ try
 
     % Create and configure the runner
     runner = TestRunner.withTextOutput('Verbosity',3);
-    artifactsFolder = fullfile(ws,'artifacts');
-    mkdir(artifactsFolder);
-    runner.ArtifactsRootFolder = artifactsFolder;
-    
-
 
     % Add the TAP plugin
     tapFile = fullfile(ws, 'testResults.tap');
     runner.addPlugin(TAPPlugin.producingVersion13(ToFile(tapFile)));
-   % runner.addPlugin(TAPPlugin.producingOriginalFormat(ToFile(tapFile)));
-    
-   runner.addPlugin(XMLPlugin.producingJUnitFormat('testResults.xml'));
-   
-   % Add the TestReportPlugin
-    % pdf
-    pdfFile = fullfile(ws, 'TestReport.pdf');
-    runner.addPlugin(TestReportPlugin.producingPDF(pdfFile));
-    
-    % html
-    htmlFolder = fullfile(ws, 'testresults');
-    runner.addPlugin(TestReportPlugin.producingHTML(htmlFolder,...
-        'IncludingCommandWindowText', true,  'IncludingPassingDiagnostics', true));
-    
-    
-    % Add the CodeCoveragePlugin
-    srcFolder = fullfile(ws, 'source');
-    coverageFile = fullfile(ws, 'coverage.xml');
-    runner.addPlugin(CodeCoveragePlugin.forFolder(srcFolder,...
-        'Producing', CoberturaFormat(coverageFile)));
-    
-    
+     
     results = runner.run(suite)
 catch e
     disp(getReport(e,'extended'));
