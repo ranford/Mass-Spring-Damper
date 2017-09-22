de4acc4c117ad0c1cdc2d309a4bd26a9edfb6ae3
Andy Campbell <ranford.b.crandal@gmail.com>
2017-09-22 11:06:58 -0400

    Use local function

--- source/springMassDamperDesign.m
+++ source/springMassDamperDesign.m
@@ -1,6 +1,8 @@
-m = 1500; % Need to know the mass to determine critical damping
+
 
 design.k = 5e6;                   % Spring Constant
-design.c = 2*m*sqrt(design.k/m); % Damping Coefficient to be critically damped
+design.c = 2*getMass*sqrt(design.k/getMass); % Damping Coefficient to be critically damped
 
-clear m;
+function m = getMass
+m = 1500; % Need to know the mass to determine critical damping
+end
