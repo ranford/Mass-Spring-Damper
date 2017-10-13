d1e0683707118759b0575a89b4e8676180b22cfe
andycampbell <andycampbell@andys-mbp-wifi.dhcp.mathworks.com>
2017-10-13 15:42:10 -0400

    Add coverage

--- runALL_THE_TESTS.m
+++ runALL_THE_TESTS.m
@@ -2,7 +2,8 @@ try
     import('matlab.unittest.TestRunner');
     import('matlab.unittest.plugins.XMLPlugin');
     import('matlab.unittest.plugins.ToFile');
-
+    import('matlab.unittest.plugins.CodeCoveragePlugin');
+    import('matlab.unittest.plugins.codecoverage.CoberturaFormat');
     
     ws = getenv('WORKSPACE');
     
@@ -18,12 +19,17 @@ try
     % Add the TAP plugin
     resultsDir = fullfile(ws, 'testresults');
     mkdir(resultsDir);
+    
     resultsFile = fullfile(resultsDir, 'testResults.xml');
     runner.addPlugin(XMLPlugin.producingJUnitFormat(resultsFile));
-     
-    results = runner.run(suite)
+
+    coverageFile = fullfile(resultsDir, 'coverage.xml');
+    runner.addPlugin(CodeCoveragePlugin.forFolder(src,...
+        'Producing', CoberturaFormat(coverageFile)));
+    
+    results = runner.run(suite) 
 catch e
     disp(getReport(e,'extended'));
-    exit(1);
+   % exit(1);
 end
-quit('force');
+%quit('force');
