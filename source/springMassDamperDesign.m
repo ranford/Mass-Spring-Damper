42e5e7a9fe3319a6576108619936a0fd5840aceb
andycampbell <andycampbell@andys-mbp-wifi.dhcp.mathworks.com>
2017-10-13 15:57:36 -0400

    Revert to script

--- source/springMassDamperDesign.m
+++ source/springMassDamperDesign.m
@@ -1,6 +1,6 @@
-design.k = 5e6;                   % Spring Constant
-design.c = 2*getMass*sqrt(design.k/getMass); % Damping Coefficient to be critically damped
-
-function m = getMass
 m = 1500; % Need to know the mass to determine critical damping
-end
+
+design.k = 5e6;                   % Spring Constant
+design.c = 2*m*sqrt(design.k/m); % Damping Coefficient to be critically damped
+
+clear m;
