65f7d4b0e1712a48c1dc596ee4b644fe1ac1eb16
andycampbell <andycampbell@andys-mbp-wifi.dhcp.mathworks.com>
2017-10-13 16:26:39 -0400

    Only add coverage past 17b.

---
+++ addCoberturaCoverageIfPossible.m
@@ -0,0 +1,11 @@
+function addCoberturaCoverageIfPossible(runner, coverageFile)
+
+if ~verLessThan('matlab','9.3')
+    import('matlab.unittest.plugins.CodeCoveragePlugin');
+    import('matlab.unittest.plugins.codecoverage.CoberturaFormat');
+    
+    runner.addPlugin(CodeCoveragePlugin.forFolder(src,...
+        'Producing', CoberturaFormat(coverageFile)));
+end
+
+end
