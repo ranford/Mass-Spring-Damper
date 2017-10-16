c7990c2b3d8f32989a2aaf6c7aed4c90b5a0c20f
andycampbell <andycampbell@andys-mbp-wifi.dhcp.mathworks.com>
2017-10-16 16:56:26 -0400

    Change design script to a function

--- tests/designTest.m
+++ tests/designTest.m
@@ -5,7 +5,7 @@ end
 function testSettlingTime(testCase) 
 %%Test that the system settles to within 0.001 of zero under 2 seconds.
 
-[position, time] = simulateSystem; 
+[position, time] = simulateSystem(springMassDamperDesign); 
 
 positionAfterSettling = position(time > .002);
 
@@ -16,8 +16,15 @@ end
 function testOvershoot(testCase)
  %Test to ensure that overshoot is less than 0.01
 
-[position, time] = simulateSystem;
+[position, ~] = simulateSystem(springMassDamperDesign);
 overshoot = max(position);
 
 verifyLessThan(testCase, overshoot, 0.01);
+end
+
+function testInvalidInput(testCase)
+% Test to ensure we fail gracefully with bogus input
+
+testCase.verifyError(@() simulateSystem('bunk'), ...
+    'simulateSystem:InvalidDesign:ShouldBeStruct');
 end
