02c9aebed546dbf3034ff7c5da70d7ce95e6fdff
Andy Campbell <ranford.b.crandal@gmail.com>
2018-01-15 12:37:30 -0500

    Update springMassDamperDesign.m

--- source/springMassDamperDesign.m
+++ source/springMassDamperDesign.m
@@ -1,5 +1,10 @@
-function design = springMassDamperDesign
-m = 1500; % Need to know the mass to determine critical damping
+function design = springMassDamperDesign(mass)
+
+if nargin
+  m = mass;
+else
+  m = 1500; % Need to know the mass to determine critical damping
+end
 
 design.k = 5e6;                  % Spring Constant
 design.c = 2*m*sqrt(design.k/m); % Damping Coefficient to be critically damped
