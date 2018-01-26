edbeeb42f6ff79eb864aa06f3cd4d705c502831d
Andy Campbell <ranford.b.crandal@gmail.com>
2018-01-26 06:43:27 -0500

    adjsut

--- source/springMassDamperDesign.m
+++ source/springMassDamperDesign.m
@@ -6,5 +6,5 @@ else
   m = 1500; % Need to know the mass to determine critical damping
 end
 
-design.k = 5e6;                  % Spring Constant
+design.k = .5e6;                  % Spring Constant
 design.c = 2*m*sqrt(design.k/m); % Damping Coefficient to be critically damped
