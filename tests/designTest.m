93fda8c32db4a741d0bb32c3a8d0102c0261f60e
mcafaro <mark.cafaro@mathworks.com>
2018-03-31 21:24:02 -0400

    Added parameterized tests

--- tests/designTest.m
+++ tests/designTest.m
@@ -1,30 +1,35 @@
-function tests = designTest
-tests = functiontests(localfunctions); 
-end
+classdef designTest < matlab.unittest.TestCase
 
-function testSettlingTime(testCase) 
-%%Test that the system settles to within 0.001 of zero under 2 seconds.
+    properties(TestParameter)
+        mass = struct('LightTruck', 1500, 'HeavyDuty', 2500);
+    end
 
-[position, time] = simulateSystem(springMassDamperDesign); 
+    methods(Test)
+        function testSettlingTime(testCase, mass) 
+            %%Test that the system settles to within 0.001 of zero under 2 seconds.
 
-positionAfterSettling = position(time > .002);
+            [position, time] = simulateSystem(springMassDamperDesign(mass)); 
+            positionAfterSettling = position(time > .002);
 
-%For this example, verify the first value after the settling time.
-verifyLessThan(testCase, abs(positionAfterSettling), 2);
-end
+            %For this example, verify the first value after the settling time.
+            verifyLessThan(testCase, abs(positionAfterSettling), 2);
+        end
 
-function testOvershoot(testCase)
- %Test to ensure that overshoot is less than 0.01
+        function testOvershoot(testCase, mass)
+             %Test to ensure that overshoot is less than 0.01
 
-[position, ~] = simulateSystem(springMassDamperDesign);
-overshoot = max(position);
+            [position, ~] = simulateSystem(springMassDamperDesign(mass));
+            overshoot = max(position);
 
-verifyLessThan(testCase, overshoot, 0.01);
-end
+            verifyLessThan(testCase, overshoot, 0.01);
+        end
 
-function testInvalidInput(testCase)
-% Test to ensure we fail gracefully with bogus input
+        function testInvalidInput(testCase)
+            % Test to ensure we fail gracefully with bogus input
 
-testCase.verifyError(@() simulateSystem('bunk'), ...
-   'simulateSystem:InvalidDesign:ShouldBeStruct');
+            testCase.verifyError(@() simulateSystem('bunk'), ...
+               'simulateSystem:InvalidDesign:ShouldBeStruct');
+        end
+    end
+    
 end
