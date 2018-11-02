b934de7717432e0d6b964bbf7bdfb792ad0d52d9
Andy Campbell <ranford.b.crandal@gmail.com>
2018-11-02 12:57:12 -0400

    Update runALL_THE_TESTS.m

--- runALL_THE_TESTS.m
+++ runALL_THE_TESTS.m
@@ -1,32 +1,25 @@
 import matlab.unittest.*;
 import matlab.unittest.plugins.*;
-try
-   
-    ws = getenv('WORKSPACE');
+ws = getenv('WORKSPACE');
     
-    src = fullfile(ws, 'source');
-    addpath(src);
+src = fullfile(ws, 'source');
+addpath(src);
 
-    % Create and configure the runner
-    runner = TestRunner.withTextOutput('Verbosity',3);
+% Create and configure the runner
+runner = TestRunner.withTextOutput('Verbosity',3);
 
-    % Add the TAP plugin
-    resultsDir = fullfile(ws, 'testresults');
-    mkdir(resultsDir);
+% Add the TAP plugin
+resultsDir = fullfile(ws, 'testresults');
+mkdir(resultsDir);
     
-    resultsFile = fullfile(resultsDir, 'testResults.tap');
-    runner.addPlugin(TAPPlugin.producingVersion13(ToFile(resultsFile)));
+resultsFile = fullfile(resultsDir, 'testResults.tap');
+runner.addPlugin(TAPPlugin.producingVersion13(ToFile(resultsFile)));
    
-    mkdir('reports')
-    runner.addPlugin(TestReportPlugin.producingHTML('reports'));
+mkdir('reports')
+runner.addPlugin(TestReportPlugin.producingHTML('reports'));
     
     %coverageFile = fullfile(resultsDir, 'cobertura.xml');
     
     %addCoberturaCoverageIfPossible(runner, src, coverageFile);
     
-    runner.run(testsuite(pwd,'IncludeSubfolders',true));
-catch e
-    disp(getReport(e,'extended'));
-    exit(1);
-end
-quit('force');
+runner.run(testsuite(pwd,'IncludeSubfolders',true));
